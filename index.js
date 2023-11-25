985365fcb36801c1d6f870359a2b4c1fd5795e09
samadjon <samadjon@gmail.com>
2023-11-25 14:15:06 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let inp=document.getElementById("inp")
+let h1=document.getElementById("h1")
+
+let arr = [];
+
+
+function ok(){
+    arr.push[inp.value];
+
+    for ( i=0; i<arr.length;i++)  {
+        h1.innerText===arr
+    };
+};
+
+function xcha(){
+    arr.pop 
+};
